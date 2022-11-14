1ee73813f29f4a9ef10afdebc6ebe86b31779ac0
wisdomdzontoh <wisdomdzontoh@gmail.com>
2022-11-14 09:41:01 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Module: 1-my_list '''
+
+
+class MyList(list):
+    ''' Represents a MyList '''
+
+    def print_sorted(self):
+        ''' prints the list, but sorted '''
+        print(sorted(self))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
